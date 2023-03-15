c0fd349b0b8e14c132f3038125781e8fba1da4e8
jk <jkt@jkui-iMac.local>
2023-03-16 01:03:35 +0900

    5. Transfers

--- test/Token.js
+++ test/Token.js
@@ -8,7 +8,8 @@ const tokens = (n) => {
 describe ('Token', () => {
 	let token, 
 		accounts, 
-		deployer;
+		deployer,
+		receiver
 
 	beforeEach(async () => {
 		const Token = await ethers.getContractFactory('Token')
@@ -19,6 +20,7 @@ describe ('Token', () => {
 		)
 		accounts = await ethers.getSigners()
 		deployer = await accounts[0]
+		receiver = await accounts[1]
 	});
 
 	describe('Deployment', () => {
@@ -48,7 +50,51 @@ describe ('Token', () => {
 		});
 	})
 
+	describe('Sending Tokens', () => {
+		let amount,
+			transaction,
+			result
+		describe('Sucess', () => {
+			beforeEach(async () => {
+				amount = tokens(100)
+				transaction = await token.connect(deployer).transfer(receiver.address, amount)
+				result = await transaction.wait()
+			})
+			
+			it('Transfers token balances', async () => {
+				expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900))
+				expect(await token.balanceOf(receiver.address)).to.equal(amount)
+			})
 
+			it('Emits a Transfer event', async () => {
+				const event = result.events[0]
+				expect(event.event).to.equal('Transfer')
+
+				const args = event.args
+				expect(args.from).to.equal(deployer.address)
+				expect(args.to).to.equal(receiver.address)
+				expect(args.value).to.equal(amount)
+
+			})
+		})
+
+		describe('Failure', () => {
+			
+			it('Rejects insufficient balances', async () => {
+				const invalidAmount = tokens(100000000)
+				await expect(token.connect(deployer).transfer(receiver.address, invalidAmount))
+				.to.be.reverted
+			})
+
+			it('Rejects invalid recipent', async () => {
+				const amount = tokens(100)
+				await expect(token.connect(deployer).transfer('0x0000000000000000000000000000000000000000', amount))
+				.to.be.reverted
+				
+			})
+		})
+		
+	})
 });
 
 
