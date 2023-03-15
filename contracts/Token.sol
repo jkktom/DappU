54bb84b2148927240aa4d6677110dd6655ee8c3d
jk <jkt@jkui-iMac.local>
2023-03-15 17:53:06 +0900

    4. Balances

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -10,6 +10,8 @@ contract Token {
 	uint256 public decimals = 18;
 	uint256 public totalSupply;
 
+	mapping(address => uint256) public balanceOf;
+		
 	constructor(
 		string memory _name, 
 		string memory _symbol,
@@ -18,12 +20,7 @@ contract Token {
 		name = _name;
 		symbol = _symbol;
 		totalSupply = _totalSupply * (10**decimals);
+		balanceOf[msg.sender] = totalSupply;
 	}
 
 }
-/*
-string public name;								
-	string public symbol = "DAPP";
-	uint256 public decimals = 18;
-	uint256 public totalSupply = 1000000 * (10**decimals);
-*/
--- test/Token.js
+++ test/Token.js
@@ -6,7 +6,9 @@ const tokens = (n) => {
 }
 
 describe ('Token', () => {
-	let token;
+	let token, 
+		accounts, 
+		deployer;
 
 	beforeEach(async () => {
 		const Token = await ethers.getContractFactory('Token')
@@ -15,6 +17,8 @@ describe ('Token', () => {
 			'DAPP',
 			'1000000'
 		)
+		accounts = await ethers.getSigners()
+		deployer = await accounts[0]
 	});
 
 	describe('Deployment', () => {
@@ -38,7 +42,13 @@ describe ('Token', () => {
 		it ('has correct total supply', async () => {
 			expect(await token.totalSupply()).to.equal(totalSupply);
 		});
+		
+		it ('assigns total supply to deployer', async () => {
+			expect(await token.balanceOf(deployer.address)).to.equal(totalSupply);
+		});
 	})
+
+
 });
 
 
