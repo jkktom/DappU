9e34b9494668983f3e4a6915afd59aab6c7e7978
jk <jkt@jkui-iMac.local>
2023-03-19 10:56:48 +0900

    8. Deployment

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -3,7 +3,8 @@ async function main() {
   const Token = await ethers.getContractFactory("Token")
 
   //Deploy contract
-  const token = await Token.deploy()
+  const token = await Token.deploy(
+    'Dapp University', 'DAPP', '1000000')
   await token.deployed()
   console.log(`Token Deployed to : ${token.address}`)
 }
@@ -11,8 +12,5 @@ async function main() {
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main()
-  .then(() => process.exit(0)
-  .catch((error) => {
-  console.error(error);
-  process.exit(1);
-});
+  .then(() => process.exit(0))
+  .catch((error) => {console.error(error);process.exit(1);});
